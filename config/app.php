1bdf1b7aefe7846fdb0849d6616128720da59dad
Soporte TesLTDA <gerencia0tesltda@gmail.com>
2025-09-16 10:56:56 -0500

    feat: agregar soporte para PHP 8.4 y limpiar provider de Cloudflare

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\CloudflareOptimizationProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\Filament\EchoAdminPanelProvider::class,
